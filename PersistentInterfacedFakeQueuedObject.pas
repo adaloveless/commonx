906bc1e80a7215e1df13decbd778cfe8f0ed4e34
Jason Nelson <jnelson@zivix.net>
2018-11-24 10:48:57 -0600

    add PersistentInterfacedFakeQueuedObject

---
+++ PersistentInterfacedFakeQueuedObject.pas
@@ -0,0 +1,163 @@
+unit PersistentInterfacedFakeQueuedObject;
+//This unit contains the TPersistentInterfacedObject class.
+
+interface
+
+uses
+  ComObj, CommonInterfaces, Windows, fakeLockQueue;
+
+{$D+}
+
+type
+//------------------------------------------------------------------------------
+  TPersistentInterfacedFakeQueuedObject = class(TFakeLockQueue, IUnknownEnhanced)
+    //TPersistentInterfacedObject is like a hybird of the VCL TInterfacedObject
+    //and a standard TObject funtionality.  TPersistentInterfaced object supports
+    //interface reference counting, but unlike TInterfacedObject, a class
+    //descending from TPersistentInterfacedObject will NOT de destroyed
+    //automatically when the reference count goes to zero.  The object, just
+    //normal objects, will not go away until DESTROY/FREE is called. If DESTROY
+    //is called while there are still references to the object, the object will
+    //wait until the references go away to be completely destroyed.
+
+    protected
+      FCOMReferences: integer;
+        //number of interfaces currently opened to the object
+      bInternalRef: boolean;
+        //signifies whether the object holds a reference to itself
+      bFreeAtWill : boolean;
+      sectCOM : _RTL_CRITICAL_SECTION;
+    public
+      procedure BeforeDestruction; override;
+      function QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
+      function _AddRef: Integer; override;stdcall;
+      function _Release: Integer; override;stdcall;
+      constructor Create; reintroduce; virtual;
+      procedure ReleaseInternalRef;
+      destructor Destroy; override;
+      procedure FreeAtWill; virtual;
+      procedure FreeWithReferences; virtual;
+      procedure Free; reintroduce; virtual;
+      property COMReferences: integer read FComReferences;
+  end;
+//------------------------------------------------------------------------------
+
+implementation
+
+uses
+  SysUtils, systemx;
+
+//------------------------------------------------------------------------------
+constructor TPersistentInterfacedFakeQueuedObject.Create;
+begin
+  inherited;
+  InitializeCriticalSection(sectCOM);
+
+  //upon initialization, the object holds a reference to itself
+  bInternalRef := true;
+  //FComReferences := 1;
+end;
+//------------------------------------------------------------------------------
+procedure TPersistentInterfacedFakeQueuedObject.Free;
+begin
+  inherited;
+end;
+//------------------------------------------------------------------------------
+destructor TPersistentInterfacedFakeQueuedObject.Destroy;
+begin
+  LeaveCriticalSection(sectCOM);
+  DeleteCriticalSection(sectCOM);
+  inherited;
+end;
+//------------------------------------------------------------------------------
+procedure TPersistentInterfacedFakeQueuedObject.BeforeDestruction;
+var
+  iREf: integer;
+begin
+  if not Detached then
+    Detach;
+  //FreeAtWill cannot be set when destroy comes around... this may occur if
+  //FreeAtWill is set, but Free is still called.
+  bFreeAtWill := false;
+
+  iRef := _RefCount;
+  //Destroy is allowed if:
+    //The reference count is 1 and it is an internal reference
+    //-or-
+    //The reference count is 0 and the free was previously called
+    //(occurs when an interface reference is still pointing to the object when
+    //free is called.
+  if ((iREf = 1) and bInternalRef) or ((iRef = 0) and (not bInternalRef)) then begin
+    exit;
+  end else begin
+    if bInternalRef then begin
+      dec(FComReferences);
+      bInternalRef := false;
+    end;
+    if FComReferences > 0 then begin
+      raise EAbort.create('Aborting destruction of '+classname+' due to '+
+                      'opened COM references. Object has '+inttostr(FComReferences)+' references');
+    end;
+  end;
+  inherited BeforeDestruction;
+end;
+//------------------------------------------------------------------------------
+function TPersistentInterfacedFakeQueuedObject.QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
+//IUnknown method for returning an interface supported by an object.  If the
+//interface is not supported the E_NOINTERFACE constant is returned as the result.
+//otherwise the result is 0.
+begin
+  if GetInterface(IID, Obj) then Result := 0 else Result := E_NOINTERFACE;
+end;
+
+
+procedure TPersistentInterfacedFakeQueuedObject.ReleaseInternalRef;
+begin
+  bInternalRef := false;
+end;
+
+function TPersistentInterfacedFakeQueuedObject._AddRef: Integer;
+begin
+  result := interlockedincrement(FCOMReferences);
+
+end;
+
+function TPersistentInterfacedFakeQueuedObject._Release: Integer;
+begin
+  result := interlockeddecrement(FCOMReferences);
+  if result = 0 then
+      free;
+end;
+
+procedure TPersistentInterfacedFakeQueuedObject.FreeAtWill;
+//Causes the object to be destroyed when all references are released, but does
+//not put the object in a disonnected state. (before destruction code is not called)
+//in essence, when this procedure is called, the PersistentInterfaced object
+//acts like a standard InterfacedObject (without the persistent part)
+begin
+  bFreeAtWill := true;
+
+end;
+
+procedure TPersistentInterfacedFakeQueuedObject.FreeWithReferences;
+//IMPORTANT NOTE:  There is a difference between FreeAtWill and FreeWithReferences
+//while they both are in the same general spirit. FreeAtWill will NOT free with
+//1 reference active upon the time of the initial call
+//-- (as needed for Data field objects).
+//FreeWithReferences will act just like FREE when there is ONE reference left
+//--(as needed by TDataObjectCache);
+begin
+  if _RefCount = 1 then begin
+    if bInternalRef then begin
+      bInternalRef := false;
+      _Release
+    end;
+
+  end else
+    bFreeAtWill := true;
+
+end;
+
+
+end.
+
